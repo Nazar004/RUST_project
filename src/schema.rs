2c7c907824228b983ca5fdf4030dc0e7817496be
Kyrylenko Yuliia <kirilenko.julia04@gmail.com>
2025-05-07 16:38:33 +0200

    Update schema.rs

--- src/schema.rs
+++ src/schema.rs
@@ -7,3 +7,21 @@ table! {
         password -> Varchar,
     }
 }
+
+
+table! {
+    transactions (tran_type, user_id, tran_id) {
+        tran_type -> Varchar,
+        user_id -> Int4,
+        tran_id -> Int4,
+        tran_source -> Varchar,
+        date -> Varchar,
+        tran_amount -> Float8,
+        tran_comment -> Nullable<Varchar>,
+
+    }
+}
+
+
+joinable!(transactions -> users (user_id));
+allow_tables_to_appear_in_same_query!(users, transactions);
