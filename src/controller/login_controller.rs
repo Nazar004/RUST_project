e1c819eec44ca7a89ced9f5e6b7c6104d26fab69
Kyrylenko Yuliia <kirilenko.julia04@gmail.com>
2025-05-07 16:43:17 +0200

    Update login_controller.rs

--- src/controller/login_controller.rs
+++ src/controller/login_controller.rs
@@ -1,3 +1,5 @@
+// src/controller/login_controller.rs
+
 use diesel::r2d2::{Pool, ConnectionManager};
 use diesel::PgConnection;
 use diesel::prelude::*;
@@ -8,14 +10,16 @@ use crate::schema::users::dsl::*;
 pub fn attempt_login(
     pool: &Pool<ConnectionManager<PgConnection>>,
     auth_data: &AuthData,
-) -> Result<(), String> {
-    let mut conn = pool.get().map_err(|e| format!("DB connection error: {:?}", e))?;
-    let user_found = users
+) -> Result<i32, String> {
+    let mut conn = pool
+        .get()
+        .map_err(|e| format!("DB connection error: {:?}", e))?;
+    let user: User = users
         .filter(username.eq(&auth_data.username))
-        .first::<User>(&mut conn)
+        .first(&mut conn)
         .map_err(|_| "User not found".to_string())?;
-    if verify(&auth_data.password, &user_found.password).unwrap_or(false) {
-        Ok(())
+    if verify(&auth_data.password, &user.password).unwrap_or(false) {
+        Ok(user.id)
     } else {
         Err("Invalid password".to_string())
     }
